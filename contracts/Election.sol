1d6b3efb9e67a750d707f3ca2c526ec37a604410
kristiantapia <kristian.tapia@blockchaingroup.io>
2018-10-24 18:25:33 -0700

    add voting capabilities

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -10,6 +10,9 @@ contract Election {
         uint voteCount;
     }
 
+    //Store accounts that have voted
+    mapping(address => bool) public voters;
+
     //Store Candidates
     mapping(uint => Candidate) public candidates;
 
@@ -28,5 +31,19 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        //require that address has not voted 
+        require(!voters[msg.sender], " this account has voted already!");
+
+        //require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount, " invalid candidate!");
+
+        //Record that voter as voted 
+        voters[msg.sender] = true;
+
+        //Update candidate vote count
+        candidates[_candidateId].voteCount ++;
     }
 }
